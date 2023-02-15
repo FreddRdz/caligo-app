9029affcffaeb849ca7bc8d47ffaf74f60fc0b58
Alfredo Rodriguez <fredd.073100@gmail.com>
2023-02-15 17:54:15 -0600

    Initial Commit

---
+++ CaligoApp/CaligoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaligoAppApp.swift
+//  CaligoApp
+//
+//  Created by Alfredo Rodriguez Coronado on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CaligoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
